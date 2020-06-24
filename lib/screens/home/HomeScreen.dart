2a604f64f96a5026d08e48f8269886254e0187c7
Sachin Khatrani <sachinkhatrani25@gmail.com>
2020-06-25 02:00:11 +0530

    second commit

--- lib/screens/home/HomeScreen.dart
+++ lib/screens/home/HomeScreen.dart
@@ -16,6 +16,60 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         backgroundColor: Color(0xffFBB034),
       ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height*0.5,
+          padding: EdgeInsets.only(top: 25, bottom: 30),
+          margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black,
+                  offset: Offset(1, 2),
+                  spreadRadius: 1.0,
+                  blurRadius: 5.0)
+            ],
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+          ),
+
+          child:Column(
+            children: <Widget>[
+              new Center(
+                child: new Image.asset(
+                  'assets/logo.png',
+                  width: 200,
+                  height: 200,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(
+                  Icons.check_circle,
+                  color: Colors.green,
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RichText(
+                  // RichText is used to styling a particular text span in a text by grouping them in one widget
+                  text: TextSpan(
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18
+                    ),
+                    text: 'First Challenge \n   Completed',
+
+
+
+                  )
+                )
+              )
+            ],
+          ),
+      ),
     );
   }
 }
